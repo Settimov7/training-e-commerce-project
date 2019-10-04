50a249a2f57969e147e0417f1bfc6925ba83a275
Прытков Никита Сергеевич <Nikita_Prytkov@epam.com>
2019-10-04 16:18:00 +0400

    add routing

--- src/components/app/app.component.tsx
+++ src/components/app/app.component.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
 
 import {HomePage} from '../../pages/homepage/homepage.component';
 
 import './app.styles.scss';
 
+const HatsPage: React.FC = () => <div>HATS PAGE</div>;
+
 export const App: React.FC = () => {
     return (
-        <div>
-            <HomePage/>
-        </div>
+        <Switch>
+            <Route exact path='/' component={HomePage}/>
+            <Route path='/hats' component={HatsPage}/>
+        </Switch>
     );
 };
