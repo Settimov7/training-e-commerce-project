eeaa4ecaf3b5323d11636342f75fccc4ae37223c
Прытков Никита Сергеевич <Nikita_Prytkov@epam.com>
2019-11-06 15:24:23 +0400

    add header component

--- src/components/app/app.component.tsx
+++ src/components/app/app.component.tsx
@@ -3,14 +3,19 @@ import {Switch, Route} from 'react-router-dom';
 
 import {HomePage} from '../../pages/homepage/homepage.component';
 import {ShopPage} from '../../pages/shop/shop.component';
+import {Header} from '../header/header.component';
 
 import './app.styles.scss';
 
 export const App: React.FC = () => {
     return (
-        <Switch>
-            <Route exact path='/' component={HomePage}/>
-            <Route path='/shop' component={ShopPage}/>
-        </Switch>
+        <div>
+            <Header/>
+
+            <Switch>
+                <Route exact path='/' component={HomePage}/>
+                <Route path='/shop' component={ShopPage}/>
+            </Switch>
+        </div>
     );
 };
