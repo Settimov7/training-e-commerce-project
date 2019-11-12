46ebf2f16eee811454fc522c331a02346e6f9c69
Nikita Prytkov <prytkov.nikita.set@gmail.com>
2019-11-12 12:44:07 +0400

    add subscribe to auth

--- src/components/app/app.component.tsx
+++ src/components/app/app.component.tsx
@@ -1,23 +1,54 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
+import {Unsubscribe, User} from 'firebase';
 
 import {Header} from '../header/header.component';
 import {HomePage} from '../../pages/homepage/homepage.component';
 import {ShopPage} from '../../pages/shop/shop.component';
 import {SignInAndSignUpPage} from '../../pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
+import {auth} from '../../firebase/firebase.utils';
+
 import './app.styles.scss';
 
-export const App: React.FC = () => {
-    return (
-        <div>
-            <Header/>
+type Props = {};
 
-            <Switch>
-                <Route exact path='/' component={HomePage}/>
-                <Route path='/shop' component={ShopPage}/>
-                <Route path='/sign-in' component={SignInAndSignUpPage}/>
-            </Switch>
-        </div>
-    );
+type State = {
+  currentUser: User | null,
 };
+
+export class App extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+
+        this.state = {
+            currentUser: null
+        }
+    };
+
+    unsubscribeFromAuth: Unsubscribe | null = null;
+
+    componentDidMount(): void {
+        this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+            this.setState({currentUser: user})
+        })
+    }
+
+    componentWillUnmount(): void {
+        this.unsubscribeFromAuth && this.unsubscribeFromAuth();
+    }
+
+    render() {
+        return (
+            <div>
+                <Header/>
+
+                <Switch>
+                    <Route exact path='/' component={HomePage}/>
+                    <Route path='/shop' component={ShopPage}/>
+                    <Route path='/sign-in' component={SignInAndSignUpPage}/>
+                </Switch>
+            </div>
+        );
+    }
+}
