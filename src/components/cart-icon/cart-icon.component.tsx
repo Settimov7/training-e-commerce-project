3bd11fdef5643732cc6e42c98e3c82adaf2aaf5e
Nikita Prytkov <prytkov.nikita.set@gmail.com>
2019-11-30 09:43:23 +0400

    adds display item count in cart icon component

--- src/components/cart-icon/cart-icon.component.tsx
+++ src/components/cart-icon/cart-icon.component.tsx
@@ -1,29 +1,38 @@
 import React from 'react';
-import {connect, MapDispatchToProps} from 'react-redux';
+import {connect, MapDispatchToProps, MapStateToProps} from 'react-redux';
 
 import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
 
 import {toggleCartHidden} from '../../redux/cart/cart.actions';
 
 import './cart-icon.styles.scss';
+import {State} from "../../redux/types";
 
-const CartIconView: React.FC<Props> = ({ toggleCartHidden }) => (
+const CartIconView: React.FC<Props> = ({ toggleCartHidden, itemCount }) => (
     <div className='cart-icon' onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon'/>
-        <span className='item-count'>0</span>
+        <span className='item-count'>{itemCount}</span>
     </div>
 );
 
 type OwnProps = {};
 
+type StateProps = {
+    itemCount: number,
+};
+
 type DispatchProps = {
     toggleCartHidden: typeof toggleCartHidden,
 }
 
-type Props = OwnProps & DispatchProps;
+type Props = OwnProps & StateProps & DispatchProps;
+
+const mapStateToProps: MapStateToProps<StateProps, OwnProps, State> = (state) => ({
+    itemCount: state.cart.cartItems.reduce((count, item) => count + item.quantity, 0),
+});
 
 const mapDispatchToProps: MapDispatchToProps<DispatchProps, OwnProps> = ({
     toggleCartHidden,
 });
 
-export const CartIcon = connect(null, mapDispatchToProps)(CartIconView);
+export const CartIcon = connect<StateProps, DispatchProps, OwnProps, State>(mapStateToProps, mapDispatchToProps)(CartIconView);
