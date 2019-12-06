84d5a3ecff31d89c4fe5b57517d16a5c684fe228
Nikita Prytkov <prytkov.nikita.set@gmail.com>
2019-12-06 15:21:09 +0400

    adds redux-persist

--- src/redux/root-reducer.ts
+++ src/redux/root-reducer.ts
@@ -1,13 +1,24 @@
 import {combineReducers} from 'redux';
+import {persistReducer} from 'redux-persist';
+import storage from 'redux-persist/es/storage';
+import {PersistConfig} from 'redux-persist/es/types';
 
 import {userReducer} from './user/user.reducer';
 import {cartReducer} from './cart/cart.reducer';
 
 import {State} from './types';
 
+const persistConfig: PersistConfig<State> = {
+    key: 'root',
+    storage,
+    whitelist: ['cart'],
+};
+
 //TODO: Разобраться с ошибкой в combineReducers
 
-export const rootReducer = combineReducers<State>({
+const reducer = combineReducers<State>({
     user: userReducer,
     cart: cartReducer,
 });
+
+export const rootReducer = persistReducer<State>(persistConfig, reducer);
